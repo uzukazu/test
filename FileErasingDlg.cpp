8c3374edd2964fbdb8d4ba2257da25d0df21b416
uzukazu <txphd466@ybb.ne.jp>
2019-05-14 17:35:58 +0900

    ad

--- FileErasingDlg.cpp
+++ FileErasingDlg.cpp
@@ -356,6 +356,7 @@ void CFileErasingDlg::FindFile(CString folder,CStringArray &extension, int findC
 	
 	hFind = FindFirstFile(strSeek, &win32fd);
 
+
 	if (hFind == INVALID_HANDLE_VALUE) {
 		AfxMessageBox(_T("hFind==INVALID_HANDLE_VALUE"));
 		return;
